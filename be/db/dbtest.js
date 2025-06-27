1336d8083e66716622ac681f9a0232abfc741177
ssimdedos <sjk3037@gmail.com>
2025-06-27 15:00:14 +0900

    db 초기화

--- be/db/dbtest.js
+++ be/db/dbtest.js
@@ -1,9 +1,15 @@
-const sql = require('sqlite3');
-const path = require('path');
-
+// const sql = require('sqlite3');
+// const path = require('path');
+// const bcrypt = require('bcryptjs');
+import sqlite3 from 'sqlite3';
+import path from 'path';
+import * as bcrypt from 'bcryptjs';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const db_path = path.resolve(__dirname, 'blog.db');
 
-const db = new sql.Database(db_path, (err) => {
+const db = new sqlite3.Database(db_path, (err) => {
   if (err) console.log(err);
   else console.log('sqlite db 연결 성공');
 });
@@ -32,25 +38,24 @@ db.run(`CREATE TABLE IF NOT EXISTS posts (
   view_count INTEGER DEFAULT 0,
   like_count INTEGER DEFAULT 0,
   order_index INTEGER DEFAULT 0,
-  FOREIGN KEY (category_id) REFERENCES categories(id),
-  FOREIGN KEY (sub_category_id) REFERENCES subcategories(id)
+  FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
+  FOREIGN KEY (sub_category_id) REFERENCES subcategories(id) ON DELETE SET DEFAULT
+)`, (err) => {
+  if (err) console.log(err);
+});
+
+db.run(`CREATE TABLE IF NOT EXISTS categories (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  is_published INTEGER DEFAULT 1,
+  order_index INTEGER DEFAULT 0
 )`, (err) => {
   if (err) console.log(err);
 });
 
 db.run(`CREATE TABLE IF NOT EXISTS tags (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL
-)`, (err) => {
-  if (err) console.log(err);
-});
-
-db.run(`CREATE TABLE IF NOT EXISTS post_tags (
-  post_id INTEGER,
-  tag_id INTEGER,
-  PRIMARY KEY (post_id, tag_id),
-  FOREIGN KEY (post_id) REFERENCES posts(id),
-  FOREIGN KEY (tag_id) REFERENCES tags(id)
+  name TEXT NOT NULL UNIQUE
 )`, (err) => {
   if (err) console.log(err);
 });
@@ -59,23 +64,19 @@ db.run(`CREATE TABLE IF NOT EXISTS subcategories (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
   category_id INTEGER,
-  FOREIGN KEY (category_id) REFERENCES categories(id)
+  order_index INTEGER DEFAULT 0,
+  FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
 )`, (err) => {
   if (err) console.log(err);
 });
 
-db.run(`CREATE TABLE IF NOT EXISTS visitors (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    visit_date TEXT NOT NULL UNIQUE,
-    unique_visitors_count INTEGER DEFAULT 0 NOT NULL,
-    today_total_page_view INTEGER DEFAULT 0 NOT NULL,
-    created_at TEXT DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%S', 'now', 'localtime')),
-    updated_at TEXT DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%S', 'now', 'localtime'))
+db.run(`CREATE TABLE IF NOT EXISTS post_tags (
+  post_id INTEGER,
+  tag_id INTEGER,
+  PRIMARY KEY (post_id, tag_id),
+  FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+  FOREIGN KEY (tag_id) REFERENCES tags(id)
 )`, (err) => {
-  if (err) console.log(err);
-});
-
-db.run(`CREATE INDEX IF NOT EXISTS idx_visitors_visit_date ON visitors (visit_date)`, (err) => {
   if (err) console.log(err);
 });
 
@@ -92,34 +93,47 @@ db.run(`CREATE TABLE IF NOT EXISTS comments (
   deleted_at TEXT DEFAULT '0',
   FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
   FOREIGN KEY (parent_comment_id) REFERENCES comments(id) ON DELETE NO ACTION
-);`, (err) => {
+)`, (err) => {
+  if (err) console.log(err);
+});
+db.run(`CREATE TABLE IF NOT EXISTS visitors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    visit_date TEXT NOT NULL UNIQUE,
+    unique_visitors_count INTEGER DEFAULT 0 NOT NULL,
+    today_total_page_view INTEGER DEFAULT 0 NOT NULL,
+    created_at TEXT DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%S', 'now', 'localtime')),
+    updated_at TEXT DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%S', 'now', 'localtime'))
+)`, (err) => {
+  if (err) console.log(err);
+});
+db.run(`CREATE INDEX IF NOT EXISTS idx_visitors_visit_date ON visitors (visit_date)
+`, (err) => {
+  if (err) console.log(err);
+});
+db.run(`CREATE TABLE IF NOT EXISTS admin (
+    id TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+`, (err) => {
   if (err) console.log(err);
 });
 
-// db.run(`DELETE FROM post_tags 
-//   WHERE rowid NOT IN ( SELECT MIN(rowid) 
-//   FROM post_tags 
-//   GROUP BY post_id, tag_id )`, (err) => {
+// const hashedPassword = await bcrypt.hash('qwertyui', parseInt(10));
+// db.run(`INSERT INTO admin (id, password) VALUES ('idea de mis dedos', ?)`, [hashedPassword], 
+//   (err) =>{
 //     if (err) console.log(err);
 //   });
 
-// db.run(``,
-//   (err) => {
-//     if (err) console.log(err);
-//   }
-// );
-
-// db.all(`SELECT * FROM visitors`, (err, rows) => {
-//   if (err) console.log('post못가져옴', err);
-//   else console.log(rows);
+// db.run(`INSERT INTO categories (name) VALUES ('삶: La vida'), ('공부: Estudio'), ('¡Aprender Coreano!'), ('기타 : etc')`, (err) => {
+//   if (err) console.log(err);
 // });
 
-db.all(`SELECT * FROM tags`, (err, rows) => {
+db.all(`SELECT * FROM categories`, (err, rows) => {
   if (err) console.log('post못가져옴', err);
   else console.log(rows);
 });
 
-db.all(`SELECT * FROM post_tags WHERE post_id = 11`, (err, rows) => {
+db.all(`SELECT * FROM admin`, (err, rows) => {
   if (err) console.log('post못가져옴', err);
   else console.log(rows);
 });
